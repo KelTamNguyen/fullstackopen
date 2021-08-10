f001c55ac87f7af1a889fbcf2aaac0b80dfda5ba
Tamperauture <60494720+Tamperauture@users.noreply.github.com>
2021-08-09 21:12:13 -0500

    complete 2.6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const handleChange = (event) => {
+    setNewName(event.target.value);
+  }
+
+  const addNewName = (event) => {
+    event.preventDefault();
+    const newPerson = { name: newName };
+    if (persons.includes(newPerson)){
+      alert(`${newPerson.name} is already added to the phonebook`);
+      console.log('success')
+    } else {
+      setPersons(persons.concat(newPerson));
+      console.log('failure')
+    }
+    setNewName('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNewName}>
+        <div>
+          name: <input value={newName} onChange={handleChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <p key={person.name}>{person.name}</p>)}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+// const Note = ({ note }) => {
+//   return (
+//     <li>{note.content}</li>
+//   )
+// }
+
+// const App = () => {
+//   const noteList = [
+//     {
+//       id: 1,
+//       content: 'HTML is easy',
+//       date: '2019-05-30T17:30:31.098Z',
+//       important: true
+//     },
+//     {
+//       id: 2,
+//       content: 'Browser can execute only JavaScript',
+//       date: '2019-05-30T18:39:34.091Z',
+//       important: false
+//     },
+//     {
+//       id: 3,
+//       content: 'GET and POST are the most important methods of HTTP protocol',
+//       date: '2019-05-30T19:20:14.298Z',
+//       important: true
+//     }
+//   ]
+//   const [notes, setNotes] = useState(noteList)
+//   const [newNote, setNewNote] = useState(
+//     'a new note...'
+//   ) 
+
+//   const addNote = (event) => {
+//     event.preventDefault()
+//     const noteObject = {
+//       content: newNote,
+//       date: new Date().toISOString(),
+//       important: Math.random() > 0.5,
+//       id: notes.length + 1,
+//     }
+    
+//     setNotes(notes.concat(noteObject))
+//     setNewNote('')
+//   }
+
+//   const handleNoteChange = (event) => {
+//     console.log(event.target.value)
+//     setNewNote(event.target.value)
+//   }
+
+//   return (
+//     <div>
+//       <h1>Notes</h1>
+//       <ul>
+//         {notes.map(note => 
+//           <Note key={note.id} note={note} />
+//         )}
+//       </ul>
+//       <form onSubmit={addNote}>
+//         <input
+//           value={newNote}
+//           onChange={handleNoteChange}
+//         />
+//         <button type="submit">save</button>
+//       </form>  
+//     </div>
+//   )
+// }
+
+// export default App

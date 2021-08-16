2ba5c8b4165ab00736b3eaea4f8c2b090d15e610
Tamperauture <60494720+Tamperauture@users.noreply.github.com>
2021-08-16 13:00:05 -0500

    completed exercise 2.13-2.14

--- part2/countries/src/components/CountryList.js
+++ part2/countries/src/components/CountryList.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import axios from 'axios';
+import { React, useState, useEffect } from 'react'
 
 export default function CountryList({ countries }) {
     return (
@@ -7,7 +8,27 @@ export default function CountryList({ countries }) {
 }
 
 const Country = ({ country }) => {
-    return (
+    const [show, setShow] = useState(false);
+    const [weather, setWeather] = useState({});
+
+    const toggleShow = () => setShow(!show);
+
+    useEffect(() => {
+        axios
+            .get(`https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&appid=${process.env.REACT_APP_API_KEY}&units=metric`)
+            .then(response => {
+                setWeather(response.data);
+            })
+    }, []);
+
+    const hiddenTemplate = (
+        <div>
+            <span>{country.name}</span>
+            <button onClick={() => toggleShow()}>show</button>
+        </div>
+    );
+
+    const showTemplate = (
         <div>
             <h1>{country.name}</h1>
             <p>capital {country.capital}</p>
@@ -17,6 +38,15 @@ const Country = ({ country }) => {
                 {country.languages.map(language => <li>{language.name}</li>)}
             </ul>
             <img src={country.flag} alt={`flag of ${country.name}`} />
+            <h1>Weather in {country.capital}</h1>
+            <p>temp: {Math.round(weather.main.temp)}&deg; C</p>
+            <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="weather-icon" />
         </div>
     )
+
+    if (show) {
+        return showTemplate;
+    } else {
+        return hiddenTemplate;
+    }
 }

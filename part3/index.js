54f683a4ea3cd26f0caaac4979a00e0513b0779c
Tamperauture <60494720+Tamperauture@users.noreply.github.com>
2021-08-20 21:27:19 -0500

    complete exercise 3.1-3.6

---
+++ part3/index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const app = express();
+app.use(express.json())
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+];
+
+app.get('/api/persons', (req, res) => {
+    console.log(persons);
+    res.json(persons);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const persons = persons.find(person => person.id === id);
+    res.json(person);
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    persons = persons.filter(person => person.id !== id);
+    
+    res.status(204).end();
+});
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body;
+    const existingPerson = persons.find(person => person.name === body.name);
+
+    if (!body.name || !boddy.number) {
+        return res.status(400).json({error: 'name or number missing'});
+    } 
+    
+    let newId = Math.floor(Math.random() * 20);
+
+    const person = {
+        id: newId,
+        name: body.name,
+        number: body.number
+    }
+
+    console.log(person);
+    persons = persons.concat(person);
+    res.json(person);
+});
+
+app.get('/api/info', (req, res) => {
+    let date = new Date();
+    res.send(`<p>Phonebook has info for ${persons.length} people</p> <br></br> ${date}`);
+});
+
+const PORT = 3001;
+app.listen(PORT);

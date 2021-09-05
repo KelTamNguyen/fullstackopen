10e678d0a3f42c6a8c7420d7e69dd2de2191a60d
Tamperauture <60494720+Tamperauture@users.noreply.github.com>
2021-09-04 21:06:52 -0500

    completed exercises 3.13-3.14

--- part3/index.js
+++ part3/index.js
@@ -1,80 +1,53 @@
-const mongoose = reuire('mongoose');
+require('dotenv').config();
+const mongoose = require('mongoose');
 const express = require('express');
 var morgan = require('morgan');
 const cors = require('cors')
 const app = express();
-
-const url = '';
+const Person = require('./models/person');
 
 app.use(express.static('build'));
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use(cors());
 
-mongoose.connect(url)
-
-morgan.token('body', (req) => JSON.stringify(req.body))
-
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-];
+morgan.token('body', (req) => JSON.stringify(req.body));
 
 app.get('/api/persons', (req, res) => {
-    console.log(persons);
-    res.json(persons);
+    Person.find({}).then(people => {
+      res.json(people)
+    });
 });
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const persons = persons.find(person => person.id === id);
-    res.json(person);
+    Person.findById(req.params.id).then(person => {
+      res.json(person);
+    });
 });
 
 app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id);
-    persons = persons.filter(person => person.id !== id);
-    
+    // const id = Number(req.params.id);
+    // persons = persons.filter(person => person.id !== id);
+    Person.remove({ id: req.params.id });
     res.status(204).end();
 });
 
 app.post('/api/persons', (req, res) => {
     const body = req.body;
-    const existingPerson = persons.find(person => person.name === body.name);
+    //const existingPerson = persons.find(person => person.name === body.name);
 
     if (!body.name || !body.number) {
         return res.status(400).json({error: 'name or number missing'});
-    } 
-
-    let newId = Math.floor(Math.random() * 20);
-
-    const person = {
-        id: newId,
-        name: body.name,
-        number: body.number
     }
 
-    console.log(person);
-    persons = persons.concat(person);
-    res.json(person);
+    const person = new Person({
+        name: body.name,
+        number: body.number
+    });
+
+    person.save().then(savedPerson => {
+      res.json(savedPerson);
+    })
 });
 
 app.get('/api/info', (req, res) => {
--- part3/mongo.js
+++ part3/mongo.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Person = require('./models/person')
 
 if (process.argv.length < 3) {
   console.log('Please provide the password as an argument: node mongo.js <password>')
@@ -8,17 +9,6 @@ if (process.argv.length < 3) {
 const password = process.argv[2]
 const name = process.argv[3]
 const number = process.argv[4]
-
-const url = `mongodb+srv://fullstack:${password}@phonebook-app.6ogig.mongodb.net/phonebook-app?retryWrites=true&w=majority`
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String
-})
-
-const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length === 3) {
   Person
